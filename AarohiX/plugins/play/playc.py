42ef9d173fffd3829280a6d896df4565d9479b68
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-05-24 20:28:32 +0300

    Update playc.py

--- AarohiX/plugins/play/playc.py
+++ AarohiX/plugins/play/playc.py
@@ -1,37 +1,33 @@
 import random
 import string
-from ast import ExceptHandler
 from strings.filters import command
 from pyrogram import filters
-from pyrogram.types import (InlineKeyboardMarkup, InputMediaPhoto,
-                            Message)
+from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
+from AarohiX import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
+from AarohiX.core.call import Dil
+from AarohiX.utils import seconds_to_min, time_to_seconds
+from AarohiX.utils.channelplay import get_channeplayCB
+from AarohiX.utils.decorators.language import languageCB
+from AarohiX.utils.decorators.play import PlayWrapper
+from AarohiX.utils.formatters import formats
+from AarohiX.utils.inline import (
+    botplaylist_markup,
+    livestream_markup,
+    playlist_markup,
+    slider_markup,
+    track_markup,
+)
+from AarohiX.utils.logger import play_logs
+from AarohiX.utils.stream.stream import stream
 from config import BANNED_USERS, lyrical
-from strings import get_command
-from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram,
-                        YouTube, app)
-from AnonX.core.call import Anon
-from AnonX.utils import seconds_to_min, time_to_seconds
-from AnonX.utils.channelplay import get_channeplayCB
-from AnonX.utils.database import is_video_allowed
-from AnonX.utils.decorators.language import languageCB
-from AnonX.utils.decorators.play import PlayWrapper
-from AnonX.utils.formatters import formats
-from AnonX.utils.inline.play import (livestream_markup,
-                                          playlist_markup,
-                                          slider_markup, track_markup)
-from AnonX.utils.inline.playlist import botplaylist_markup
-from AnonX.utils.logger import play_logs
-from AnonX.utils.stream.stream import stream
+from pyrogram.errors import UserNotParticipant, ChatAdminRequired
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 # Command
-PLAY_COMMAND = get_command("PLAY_COMMAND")
-
-
-@app.on_message(
-    command(PLAY_COMMAND)
+@app.on_message(command(["تشغيل", "alive"])
     & filters.channel
     & ~BANNED_USERS
 )
