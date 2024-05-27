6da68b6ddb66d17f4c4de1cd3e69393f1779ae68
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-05-27 12:44:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "")
 
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "bot")
 
-Muntazer = getenv("muntazer", "BBBXX4")
+Muntazer = getenv("muntazer", "vvyvv6")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 
