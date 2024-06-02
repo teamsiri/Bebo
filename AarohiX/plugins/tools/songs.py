fd880f39dde7fefc77412d83f609ec9c4a9aa678
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-06-03 01:44:29 +0300

    Update songs.py

--- AarohiX/plugins/tools/songs.py
+++ AarohiX/plugins/tools/songs.py
@@ -10,6 +10,8 @@ from AarohiX import app
 from config import SUPPORT_CHAT
 import os.path
 
+from urllib.parse import urlparse
+
 def is_valid_youtube_url(url):
     # Check if the provided URL is a valid YouTube URL
     return url.startswith(("https://www.youtube.com", "http://www.youtube.com", "youtube.com"))
@@ -29,14 +31,96 @@ async def song(_, message: Message):
         if is_valid_youtube_url(query):
             # If it's a valid YouTube URL, use it directly
             link = query
+            results = [{}]
         else:
-            # Otherwise, perform a search using the provided keyword
-            results = YoutubeSearch(query, max_results=5).to_dict()
-            if not results:
-                raise Exception("- لايوجد بحث .")
-            
-            link = f"https://youtube.com{results[0]['url_suffix']}"
+            # Check if the message contains a YouTube URL
+            parsed_url = urlparse(query)
+            if parsed_url.netloc == 'youtube.com' or parsed_url.netloc == 'www.youtube.com':
+                link = query
+                results = [{}]
+            else:
+                # Otherwise, perform a search using the provided keyword
+                results = YoutubeSearch(query, max_results=5).to_dict()
+                if not results:
+                    raise Exception("- لايوجد بحث .")
+                
+                link = f"https://youtube.com{results[0]['url_suffix']}"
 
+        title = results[0]["title"][:40]
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f"{title}.jpg"
+        # Replace invalid characters in the filename
+        thumb_name = thumb_name.replace("/", "")
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, "wb").write(thumb.content)
+        duration = results[0]["duration"]
+
+    except Exception as ex:
+        error_message = f"- فشل .\n\n**السبب :** `{ex}`"
+        return await m.edit_text(error_message)
+
+    await m.edit_text("- تم الرفع انتضر قليلاً .")
+    audio_file = ''
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+
+        rep = f"**- الأسم :** [{title[:23]}]({link})\n**- الوقت :** `{duration}`\n**- بواسطة  :** {message.from_user.first_name}"
+
+        secmul, dur, dur_arr = 1, 0, duration.split(":")
+        for i in range(len(dur_arr) - 1, -1, -1):
+            dur += int(dur_arr[i]) * secmul
+            secmul *= 60
+
+        visit_butt = InlineKeyboardMarkup(
+    [
+        [InlineKeyboardButton(text="- المنشئ .", url=SUPPORT_CHAT)],
+    ]
+)
+        # Reply to the user who initiated the search
+        await message.reply_audio(
+            audio=audio_file,
+            caption=rep,
+            thumb=thumb_name,
+            title=title,
+            duration=dur,
+            reply_markup=visit_butt,
+        )
+
+        await m.delete()
+
+    except Exception as ex:
+        error_message = f"- فشل في تحميل الفيديو من YouTube. \n\n**السبب :** `{ex}`"
+        await m.edit_text(error_message)
+
+    # Remove temporary files after audio upload
+    try:
+        if audio_file:
+            os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as ex:
+        error_message = f"- فشل في حذف الملفات المؤقتة. \n\n**السبب :** `{ex}`"
+        await m.edit_text(error_message)
+
+@app.on_message(command(["بحث"]))
+async def search_by_keyword(_, message: Message):
+    try:
+        await message.delete()
+    except:
+        pass
+    m = await message.reply_text("- يتم البحث الان .", quote=True)
+
+    query = " ".join(str(i) for i in message.command[1:])
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+
+    try:
+        results = YoutubeSearch(query, max_results=5).to_dict()
+        if not results:
+            raise Exception("- لايوجد بحث .")
+        
+        link = f"https://youtube.com{results[0]['url_suffix']}"
         title = results[0]["title"][:40]
         thumbnail = results[0]["thumbnails"][0]
         thumb_name = f"{title}.jpg"
